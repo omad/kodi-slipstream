7873de879423595117a26ae6da4c8e2499d7b17b
Damien Ayers <damien@omad.net>
2017-03-03 13:58:37 +1100

    Formatting and updates

--- gethtml.py
+++ gethtml.py
@@ -1,4 +1,4 @@
-'''
+"""
 gethtml with cookies support  v1
 by anarchintosh @ xbmcforums
 Copyleft = GNU GPL v3 (2011 onwards)
@@ -18,69 +18,87 @@ source = gethtml.get(url)
 to load html with cookies
 source = gethtml.get(url,'my-path-to-cookiefile')
 
-'''
+"""
 
-import urllib,urllib2
+import urllib, urllib2
 import cookielib
 import os
 import re
 
+# !!!!!!!!!!! Please set the compatible_urllist
+# set the list of URLs you want to load with cookies.
+# matches bits of url, so that if you want to match www243.megaupload.com/ you
+# can just put '.megaupload.com/' in the list.
+compatible_urllist = ['watchslipstream.com/']
+USER_AGENT = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3'
 
-#!!!!!!!!!!! Please set the compatible_urllist
-#set the list of URLs you want to load with cookies.
-#matches bits of url, so that if you want to match www243.megaupload.com/ you can just put '.megaupload.com/' in the list. 
-compatible_urllist = ['http://watchslipstream.com/','https://watchslipstream.com/']
+# COOKIES_PATH = os.path.join(__path__, 'cookies.txt')
+COOKIES_PATH = 'cookies.txt'
 
 
 def url_for_cookies(url):
-    #ascertain if the url contains any of the phrases in the list. return True if a match is found.
-    
+    # ascertain if the url contains any of the phrases in the list. return True if a match is found.
     for compatible_url in compatible_urllist:
-        
-        if re.search(compatible_url,url):    
-            url_is_compatible = True
-            break
+        if re.search(compatible_url, url):
+            return True
 
-        else: url_is_compatible = False
-        
-    return url_is_compatible        
+    return False
 
-def get(url,cookiepath=None):
-    #print 'processing url: '+url
 
-                 
+def myget(url, query_data=None, extra_headers=None):
+    cj = cookielib.MozillaCookieJar(COOKIES_PATH)
+    cj.load()
+
+    req = urllib2.Request(url)
+    req.add_header('User-Agent', USER_AGENT)
+    if extra_headers:
+        for k, v in extra_headers.iteritems():
+            req.add_header(k, v)
+
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    if isinstance(query_data, dict):
+        query_data = json.dumps(query_data)
+
+    response = opener.open(req, data=query_data)
+    data = response.read()
+    response.close()
+    return data
+
+
+def get(url, data=None, extra_headers=None, cookiepath=None):
+    # print 'processing url: '+url
     # use cookies if cookiepath is set and if the cookiepath exists.
     if cookiepath is not None:
 
-        #only use cookies for urls specified
-        if url_for_cookies(url) == True:
+        # only use cookies for urls specified
+        if url_for_cookies(url):
 
-                #check if user has supplied only a folder path, or a full path
-                if not os.path.isfile(cookiepath):
-                    #if the user supplied only a folder path, append on to the end of the path a common filename.
-                    cookiepath = os.path.join(cookiepath,'cookies.lwp')
+            # check if user has supplied only a folder path, or a full path
+            if os.path.isdir(cookiepath):
+                # if the user supplied only a folder path, append on to the end of the path a common filename.
+                cookiepath = os.path.join(cookiepath, 'cookies.lwp')
 
-                #check that the cookie exists
-                if os.path.exists(cookiepath):
-                 
-                    cj = cookielib.LWPCookieJar()
-                    cj.load(cookiepath)
-                    req = urllib2.Request(url)
-                    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')   
-                    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-                    response = opener.open(req)
-                    link=response.read()
-                    response.close()
-                    return link
-               
-                else: return _loadwithoutcookies(url)                
-        else: return _loadwithoutcookies(url)    
-    else: return _loadwithoutcookies(url)
+            # check that the cookie exists
+            if os.path.exists(cookiepath):
+                cj = cookielib.LWPCookieJar()
+                cj.load(cookiepath)
+
+                req = urllib2.Request(url)
+                req.add_header('User-Agent', USER_AGENT)
+
+                opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+                response = opener.open(req, data=data)
+                link = response.read()
+                response.close()
+                return link
+
+    return _loadwithoutcookies(url)
+
 
 def _loadwithoutcookies(url):
-        req = urllib2.Request(url)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')   
-        response = urllib2.urlopen(req)
-        link=response.read()
-        response.close()
-        return link  
+    req = urllib2.Request(url)
+    req.add_header('User-Agent', USER_AGENT)
+    response = urllib2.urlopen(req)
+    link = response.read()
+    response.close()
+    return link
--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from urlparse import parse_qsl
 import xbmcgui
 import xbmcplugin
 
+from .gethtml import myget
+
 # Get the plugin url in plugin:// notation.
 _url = sys.argv[0]
 
@@ -30,33 +32,7 @@ VIDEOS = {'Animals': [{'name': 'Crab',
                        'thumb': 'http://www.vidsplay.com/vids/turtle.jpg',
                        'video': 'http://www.vidsplay.com/vids/turtle.mp4',
                        'genre': 'Animals'}
-                      ],
-            'Cars': [{'name': 'Postal Truck',
-                      'thumb': 'http://www.vidsplay.com/vids/us_postal.jpg',
-                      'video': 'http://www.vidsplay.com/vids/us_postal.mp4',
-                      'genre': 'Cars'},
-                     {'name': 'Traffic',
-                      'thumb': 'http://www.vidsplay.com/vids/traffic1.jpg',
-                      'video': 'http://www.vidsplay.com/vids/traffic1.avi',
-                      'genre': 'Cars'},
-                     {'name': 'Traffic Arrows',
-                      'thumb': 'http://www.vidsplay.com/vids/traffic_arrows.jpg',
-                      'video': 'http://www.vidsplay.com/vids/traffic_arrows.mp4',
-                      'genre': 'Cars'}
-                     ],
-            'Food': [{'name': 'Chicken',
-                      'thumb': 'http://www.vidsplay.com/vids/chicken.jpg',
-                      'video': 'http://www.vidsplay.com/vids/bbqchicken.mp4',
-                      'genre': 'Food'},
-                     {'name': 'Hamburger',
-                      'thumb': 'http://www.vidsplay.com/vids/hamburger.jpg',
-                      'video': 'http://www.vidsplay.com/vids/hamburger.mp4',
-                      'genre': 'Food'},
-                     {'name': 'Pizza',
-                      'thumb': 'http://www.vidsplay.com/vids/pizza.jpg',
-                      'video': 'http://www.vidsplay.com/vids/pizza.mp4',
-                      'genre': 'Food'}
-                     ]}
+                      ]}
 
 QUERIES = {
     'BrowseQuery': """query BrowseQuery {
@@ -89,6 +65,16 @@ QUERIES = {
         __typename
     }
 }"""
+}
+
+EXTRA_HEADERS = {
+    'content-type': 'application/json',
+    'Referer': 'https://watchslipstream.com/browse',
+    'Origin': 'https://watchslipstream.com'
+}
+
+GRAPHQL_URL = 'https://watchslipstream.com/graphql'
+
 
 def get_graphql_query(query_name, **kwargs):
     return {
@@ -125,6 +111,7 @@ def get_categories():
     :return: The list of video categories
     :rtype: list
     """
+    response_data = myget(GRAPHQL_URL, get_graphql_query('BrowseQuery'), EXTRA_HEADERS)
     return VIDEOS.iterkeys()
 
 
